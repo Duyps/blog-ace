e287f816c0119c42c000c84c8060096dd752be99
Duy <quocduyps1@gmail.com>
2025-03-15 22:36:34 +0700

    sau khi sửa route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import AdminLogin from './components/admin/AdminLogin/AdminLogin';
 import AdminDashboard from './components/admin/admin-dashboard/AdminDashboard';
 import Home from './components/user/home/Home';
